7c3ff4481af37dc2d79fe17618b9ab98049aa4bb
thomasp <thomas.peeters1@telenet.be>
2018-05-04 17:51:54 +0200

    Angular-keycloak: REST services secured with Angular client application

--- backend/src/main/java/be/peeterst/tester/TesterApplication.java
+++ backend/src/main/java/be/peeterst/tester/TesterApplication.java
@@ -1,21 +1,35 @@
 package be.peeterst.tester;
 
 import be.peeterst.tester.servlet.LoggableDispatcherServlet;
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.descriptor.web.LoginConfig;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.KeycloakDeployment;
+import org.keycloak.adapters.KeycloakDeploymentBuilder;
+import org.keycloak.adapters.spi.HttpFacade;
+import org.keycloak.adapters.tomcat.KeycloakAuthenticatorValve;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.DispatcherServletAutoConfiguration;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.DispatcherServlet;
+
+import java.io.InputStream;
 
 @SpringBootApplication
 public class TesterApplication {
 
 
-    @Autowired
-    private TomcatContextCustomizer tomcatContextCustomizer;
+//    @Autowired
+//    private TomcatContextCustomizer tomcatContextCustomizer;
 
     @Bean
 	public ServletRegistrationBean dispatcherRegistration() {
@@ -32,4 +46,66 @@ public class TesterApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TesterApplication.class, args);
 	}
+
+	@Bean
+	public EmbeddedServletContainerCustomizer getKeycloakContainerCustomizer() {
+		return configurableEmbeddedServletContainer -> {
+            if (configurableEmbeddedServletContainer instanceof TomcatEmbeddedServletContainerFactory) {
+                TomcatEmbeddedServletContainerFactory container = (TomcatEmbeddedServletContainerFactory) configurableEmbeddedServletContainer;
+
+                container.addContextValves(new KeycloakAuthenticatorValve());
+
+                container.addContextCustomizers(getKeycloakContextCustomizer());
+            }
+        };
+	}
+
+    @Bean
+    public TomcatContextCustomizer getKeycloakContextCustomizer() {
+        return context -> {
+            LoginConfig loginConfig = new LoginConfig();
+            loginConfig.setAuthMethod("KEYCLOAK");
+            context.setLoginConfig(loginConfig);
+
+            context.addSecurityRole("ADMIN");
+            SecurityConstraint adminConstraint = new SecurityConstraint();
+            adminConstraint.addAuthRole("ADMIN");
+            SecurityCollection adminCollection = new SecurityCollection();
+            adminCollection.addPattern("/overview/write");
+            adminConstraint.addCollection(adminCollection);
+            context.addConstraint(adminConstraint);
+
+            context.addSecurityRole("USER");
+            SecurityConstraint userConstraint = new SecurityConstraint();
+            userConstraint.addAuthRole("USER");
+            SecurityCollection userCollection = new SecurityCollection();
+            userCollection.addPattern("/overview");
+            userConstraint.addCollection(userCollection);
+            context.addConstraint(userConstraint);
+
+            context.addParameter("keycloak.config.resolver", SpringBootKeycloakConfigResolver.class.getName());
+        };
+    }
+
+    public static class SpringBootKeycloakConfigResolver implements KeycloakConfigResolver {
+
+        private KeycloakDeployment keycloakDeployment;
+
+        @Override
+        public KeycloakDeployment resolve(HttpFacade.Request request) {
+            if (keycloakDeployment != null) {
+                return keycloakDeployment;
+            }
+
+            InputStream configInputStream = getClass().getResourceAsStream("/keycloak.json");
+            if (configInputStream == null) {
+                keycloakDeployment = new KeycloakDeployment();
+            } else {
+                keycloakDeployment = KeycloakDeploymentBuilder.build(configInputStream);
+            }
+
+            return keycloakDeployment;
+        }
+    }
+
 }
---
+++ backend/src/main/java/be/peeterst/tester/filter/AuthenticationFilter.java
@@ -0,0 +1,43 @@
+package be.peeterst.tester.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import be.peeterst.tester.auth.KeycloakAuthentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthenticationFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException,
+            ServletException {
+        //we must obtain the user from the context.
+        //CatalinaRequestAuthenticator saves the security context as request attribute
+        try {
+            SecurityContextHolder.getContext().setAuthentication(
+                    KeycloakAuthentication.KeycloakAuthenticationBuilder.buildAuthentication((HttpServletRequest)request));
+            chain.doFilter(request, response);
+        } finally {
+            SecurityContextHolder.clearContext();
+        }
+    }
+
+
+    @Override
+    public void destroy() {
+    }
+
+}
